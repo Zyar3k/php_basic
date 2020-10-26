87c2e8da7a374e652ef279d7b7b74d07e21048e3
Arkadiusz Zygan <a.zygan@gmail.com>
2020-10-26 18:24:18 +0100

    Arithmetic operators

--- index.php
+++ index.php
@@ -12,28 +12,27 @@
     text-align: center;
   }
 </style>
-  <h1>Hi php</h1>
-
   <?php
-    // if(isset($_GET['name'])){
-    //   $name = $_GET['name'];
-    // } else {
-    //   $name = 'unknown';
-    // }
-    // echo '<h2>hello, my name is ' . $name . '</h2>';
-
+    /*
+    <h4>intro</h4>
+    if(isset($_GET['name'])){
+      $name = $_GET['name'];
+    } else {
+      $name = 'unknown';
+    }
+    echo '<h2>hello, my name is ' . $name . '</h2>';
+    */
+    /*
+    <h4>zmienne, typy danych</h4>
     $var = 'Hello';
     $var = 'Hi';
     define('CONSTANT', 'World');
     echo "$var " . CONSTANT;
-
     echo '<br><br><br>';
-    
     $text = 'ABCD';
     $num = 126;
     $float = 13.31;
     $bool = false;
-
     echo "$text, $num, $float, ";
     echo '<br>';
     var_dump($bool);
@@ -42,13 +41,30 @@
     echo $textNum + $num;
     echo '<br>';
     var_dump(is_numeric($textNum));
-
-    // echo $text;
     $a = 'text';
-
     echo '<br><br>' . $a;
     echo '<br><br>' . $$a;
-    
+    */
+    echo '<h4>Operatory arytmetyczne</h4>';
+
+    $a = 10;
+    $b = 7;
+    echo "a = $a, b= $b";
+    echo "<br>Dodawanie = " . ($a + $b);
+    echo "<br>Odejmowanie = " . ($a - $b);
+    echo "<br>Mnożenie = " . ($a * $b);
+    echo "<br>Dzielenie = " . round($a / $b, 2);
+    echo "<br>Modulo = " . ($a % $b);
+    echo "<br>Potęgowanie = " . pow(2 , 3);
+    echo "<br>Pierwiastek = " . sqrt(9);
+
+    echo "<br><br>Kolejność = " . ((2 + 2) * 2);
+
+    // $a = $a +10;
+    $a += 10;
+    echo "<br><br>new a = $a";
+    $a++;
+    echo "<br><br>newer a = $a";
 
 
   ?> 
